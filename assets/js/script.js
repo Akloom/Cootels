5deef82bdcafc370c923755efc21f6872ce1fbe0
Code-Monster1 <usupovaliakbar01@gmail.com>
2023-02-26 23:13:35 +0600

    rooms done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,37 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
   navCollapse.classList.toggle('is-active');
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    740: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    920: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+  }
+});
